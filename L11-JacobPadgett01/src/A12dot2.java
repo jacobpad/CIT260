10c148207d3a43e3a90266166d04fa146fa311d9
jacobpad <jacobpad@gmail.com>
2019-07-03 09:07:45 -0700

    A12dot2 update

---
+++ L11-JacobPadgett01/src/A12dot2.java
@@ -0,0 +1,45 @@
+
+/**
+ * <h1>Jacob Padgett</h1>
+ * <h2>Brother Jones</h2>
+ * <h2>Week 10</h2>
+ * <p>CIT 260</p>
+ * <h3>12.2</h3>
+ * <p>Write a program that prompts the user to read two intigers 
+ * and displays their sum. Your program should prompt the user
+ * to read the number again if the input is incorrect.</p>
+ */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author jacob padgett
+ */
+public class A12dot2 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// Set up a do/while loop
+		int x = 1; // Acts as a boolean
+		int num1 = 0; // First number
+		int num2 = 0; // Second number
+		System.out.print("Please enter two numbers and I'll sum them for you: ");// Prompt for two numbers
+		do {
+			try {// Start the try
+				System.out.print("\nNumber 1: ");
+				num1 = input.nextInt();
+				System.out.print("Number 2: ");
+				num2 = input.nextInt();
+				System.out.println("\nSum = " + (num1 + num2));
+				x = 2; // X will change to 2 at the end of this loop, thus breaking out.
+			} catch (InputMismatchException ex) {
+				System.out.println("Error - Please make sure what you enter is two numbers - only numbers.");
+				input.nextLine();
+			}
+		} while (x == 1);
+		input.close();
+	}
+}
--- test/src/whatever.java
+++ test/src/whatever.java
@@ -1,9 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class whatever {
 	public static void main(String[] args) {
-		if (6 == 9)
-			System.out.println("yes it does equal");
-		else {
-			System.out.println("not equal");
-		}
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		boolean continueInput = true;
+
+		// Prompt the user to enter two integers
+		System.out.print("Enter two numbers: ");
+		do {
+			try {
+				int number1 = input.nextInt();
+				int number2 = input.nextInt();
+
+				// Display the result
+				System.out.println("The sum is " + (number1 + number2));
+
+				continueInput = false;
+			} catch (InputMismatchException ex) {
+				System.out.println("Try again. (" + "Incorrect input: two integers are required) ");
+				input.nextLine(); // Discard input
+			}
+		} while (continueInput);
 	}
 }
